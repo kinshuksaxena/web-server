c285c31b90e338167c60875a7411a5ea755195f7
Kinshuk Saxena <kinshuksaxena@Tanvis-MacBook-Pro.local>
2016-02-15 23:01:47 -0500

    Initialize repo

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+
+var middleware = {
+	requireAuthentication: function (req, res, next){
+		console.log('private route hit!');
+		next();
+	},
+	logger: function (req, res, next){
+		console.log('Request' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('About us!');
+}); 
+
+app.use(express.static(__dirname + '/public'));
+
+//app.use(express.static(__dirname + '/public'));
+
+app.listen(3000, function () {
+	console.log ('Express started on port ' + PORT);
+	console.log(__dirname);
+});
